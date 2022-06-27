1716070503da4fcf5b14b722d2f96d90dacafe1c
DimitriGoderidze94 <dimitrigoderidze.com>
2022-06-27 13:09:40 +0400

    Added project

--- scrypt.js
+++ scrypt.js
@@ -1,94 +1,101 @@
 let projectsList =
-    [{
-        "title": "React Store with cart",
-        "href": "https://dimitrigoderidze94.github.io/react-store/",
-        "src": "./images/Cart.png",
-        "sourceCode": "https://github.com/DimitriGoderidze94/react-store"
-    },
-    {
-        "title": "Angular covid statistics",
-        "href": "https://dimitrigoderidze94.github.io/angular-covid-statistics/",
-        "src": "./images/AngularCovidStatistics.png",
-        "sourceCode": "https://github.com/DimitriGoderidze94/angular-covid-statistics"
-    },
-    {
-        "title": "Image Search",
-        "href": "https://dimitrigoderidze94.github.io/image-search/",
-        "src": "./images/image-search.webp",
-        "sourceCode": "https://github.com/DimitriGoderidze94/image-search"
-    },
-    {
-        "title": "Emoji List",
-        "href": "https://dimitrigoderidze94.github.io/Emoji-List/",
-        "src": "./images/emoji.jpg",
-        "sourceCode": "https://github.com/DimitriGoderidze94/Emoji-List"
-    },
-    {
-        "title": "Mini Store",
-        "href": "https://dimitrigoderidze94.github.io/miniReactStore/",
-        "src": "./images/MiniStore.png",
-        "sourceCode": "https://github.com/DimitriGoderidze94/miniReactStore"
-    },
-    {
-        "title": "Random Dogs",
-        "href": "https://dimitrigoderidze94.github.io/Random-Dogs/",
-        "src": "./images/RandomDog.jfif",
-        "sourceCode": "https://github.com/DimitriGoderidze94/Random-Dogs"
-    },
-    {
-        "title": "Github Typeahead",
-        "href": "https://dimitrigoderidze94.github.io/Github-typeahead/",
-        "src": "./images/TypeAhead.png",
-        "sourceCode": "https://github.com/DimitriGoderidze94/Github-typeahead"
-    },
-    {
-        "title": "Currency Converter",
-        "href": "https://dimitrigoderidze94.github.io/Currency-Converter/",
-        "src": "./images/CurrencyConventer.png",
-        "sourceCode": "https://github.com/DimitriGoderidze94/Currency-Converter"
-    },
-    {
-        "title": "Highest grossing movies",
-        "href": "https://codepen.io/Goderidze/full/PojzWJp",
-        "src": "./images/HighestGrossingMovies.jpg",
-        "sourceCode": "https://codepen.io/Goderidze/pen/PojzWJp"
-    },
-    {
-        "title": "US Educational Attainment",
-        "href": "https://codepen.io/Goderidze/full/abwZdjJ",
-        "src": "./images/UnitedStates.png",
-        "sourceCode": "https://codepen.io/Goderidze/pen/abwZdjJ"
-    },
-    {
-        "title": "US GDP",
-        "href": "https://codepen.io/Goderidze/full/powyrZj",
-        "src": "./images/DopingBicycle.jpg",
-        "sourceCode": "https://codepen.io/Goderidze/pen/powyrZj"
-    },
-    {
-        "title": "Calculator",
-        "href": "https://codepen.io/Goderidze/full/YzVvxOv",
-        "src": "./images/calculator.png",
-        "sourceCode": "https://codepen.io/Goderidze/pen/YzVvxOv"
-    },
-    {
-        "title": "25 + 5 clock",
-        "href": "https://codepen.io/Goderidze/full/OJmEPMo",
-        "src": "./images/countDownClock.png",
-        "sourceCode": "https://codepen.io/Goderidze/pen/OJmEPMo"
-    },
-    {
-        "title": "Drum Machine",
-        "href": "https://codepen.io/Goderidze/full/wvdjPOY",
-        "src": "./images/drumMachine.png",
-        "sourceCode": "https://codepen.io/Goderidze/pen/wvdjPOY"
-    },
-    {
-        "title": "Technical Documentation Page",
-        "href": "https://codepen.io/Goderidze/full/QWvgrEZ",
-        "src": "./images/Technical-Documentation-Page.png",
-        "sourceCode": "https://codepen.io/Goderidze/pen/QWvgrEZ"
-    }
+    [
+        {
+            "title": "React Store with cart",
+            "href": "https://dimitrigoderidze94.github.io/react-store/",
+            "src": "./images/Cart.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/react-store"
+        },
+        {
+            "title": "Dashboard-2-Design",
+            "href": "https://dimitrigoderidze94.github.io/dashboard-2-design/",
+            "src": "./images/dashboard-2-design.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/dashboard-2-design"
+        },
+        {
+            "title": "Angular covid statistics",
+            "href": "https://dimitrigoderidze94.github.io/angular-covid-statistics/",
+            "src": "./images/AngularCovidStatistics.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/angular-covid-statistics"
+        },
+        {
+            "title": "Image Search",
+            "href": "https://dimitrigoderidze94.github.io/image-search/",
+            "src": "./images/image-search.webp",
+            "sourceCode": "https://github.com/DimitriGoderidze94/image-search"
+        },
+        {
+            "title": "Emoji List",
+            "href": "https://dimitrigoderidze94.github.io/Emoji-List/",
+            "src": "./images/emoji.jpg",
+            "sourceCode": "https://github.com/DimitriGoderidze94/Emoji-List"
+        },
+        {
+            "title": "Mini Store",
+            "href": "https://dimitrigoderidze94.github.io/miniReactStore/",
+            "src": "./images/MiniStore.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/miniReactStore"
+        },
+        {
+            "title": "Random Dogs",
+            "href": "https://dimitrigoderidze94.github.io/Random-Dogs/",
+            "src": "./images/RandomDog.jfif",
+            "sourceCode": "https://github.com/DimitriGoderidze94/Random-Dogs"
+        },
+        {
+            "title": "Github Typeahead",
+            "href": "https://dimitrigoderidze94.github.io/Github-typeahead/",
+            "src": "./images/TypeAhead.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/Github-typeahead"
+        },
+        {
+            "title": "Currency Converter",
+            "href": "https://dimitrigoderidze94.github.io/Currency-Converter/",
+            "src": "./images/CurrencyConventer.png",
+            "sourceCode": "https://github.com/DimitriGoderidze94/Currency-Converter"
+        },
+        {
+            "title": "Highest grossing movies",
+            "href": "https://codepen.io/Goderidze/full/PojzWJp",
+            "src": "./images/HighestGrossingMovies.jpg",
+            "sourceCode": "https://codepen.io/Goderidze/pen/PojzWJp"
+        },
+        {
+            "title": "US Educational Attainment",
+            "href": "https://codepen.io/Goderidze/full/abwZdjJ",
+            "src": "./images/UnitedStates.png",
+            "sourceCode": "https://codepen.io/Goderidze/pen/abwZdjJ"
+        },
+        {
+            "title": "US GDP",
+            "href": "https://codepen.io/Goderidze/full/powyrZj",
+            "src": "./images/DopingBicycle.jpg",
+            "sourceCode": "https://codepen.io/Goderidze/pen/powyrZj"
+        },
+        {
+            "title": "Calculator",
+            "href": "https://codepen.io/Goderidze/full/YzVvxOv",
+            "src": "./images/calculator.png",
+            "sourceCode": "https://codepen.io/Goderidze/pen/YzVvxOv"
+        },
+        {
+            "title": "25 + 5 clock",
+            "href": "https://codepen.io/Goderidze/full/OJmEPMo",
+            "src": "./images/countDownClock.png",
+            "sourceCode": "https://codepen.io/Goderidze/pen/OJmEPMo"
+        },
+        {
+            "title": "Drum Machine",
+            "href": "https://codepen.io/Goderidze/full/wvdjPOY",
+            "src": "./images/drumMachine.png",
+            "sourceCode": "https://codepen.io/Goderidze/pen/wvdjPOY"
+        },
+        {
+            "title": "Technical Documentation Page",
+            "href": "https://codepen.io/Goderidze/full/QWvgrEZ",
+            "src": "./images/Technical-Documentation-Page.png",
+            "sourceCode": "https://codepen.io/Goderidze/pen/QWvgrEZ"
+        }
     ]
 
 let projectsPage = document.createElement("div");
